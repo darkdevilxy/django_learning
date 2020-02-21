6bbe7f0bc4ed41836d8bfb96b1abd5986b3b4629
darkdevilxy <bhumagainxy@gmail.com>
2020-02-21 14:52:27 +0545

    Made change in home template Database table for posts was created

--- blog/vlog/views.py
+++ blog/vlog/views.py
@@ -2,10 +2,33 @@ from django.shortcuts import render
 from django.urls import path
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Dark Devil',
+        'title': 'Blog-Post-1',
+        'content': 'Hello world',
+        'date': 'Date'
+    },
+    {
+        'author': 'darkdevil',
+        'title': 'vlog-post-1',
+        'content': 'Hello world second time',
+        'date': 'date2'
+    }
+]
+
 
 def home(request):
-    return render(request, 'vlog/home.html')
+    context = {
+        'posts': posts,
+        'title': 'Home'
+    }
+    return render(request, 'vlog/home.html', context)
 
 
 def about(request):
-    return render(request, "vlog/about.html")
+    return render(request, "vlog/about.html", {'title': 'About'})
+
+
+def contact(request):
+    return render(request, 'vlog/contact.html', {'title': 'Contact'})
